d1b608785886422cc71586f1c6e0812de6f3d928
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-12-08 23:39:03 +0100

    Doc in its subdirectory

--- object/interface.go
+++ object/interface.go
@@ -5,8 +5,11 @@ import (
 )
 
 // IDer is any object that can give its id and serialize itself in JSON
+// START_IDER OMIT
 type IDer interface {
 	ID() string
 	json.Marshaler
 	json.Unmarshaler
 }
+
+// END_IDER OMIT
